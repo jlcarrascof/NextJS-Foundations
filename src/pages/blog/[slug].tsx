463bc756e5d969c801a8023a74b0ee6aca732ec3
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-04 12:01:59 -0400

    Refactor page.tsx to use h1 tag for dynamic slug

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -2,6 +2,5 @@ import { useRouter } from "next/router"
 
 export default function Page() {
     const router = useRouter()
-    return <p>Post: {router.query.slug}</p>
-
+    return <p>{router.query.slug}</p>
 }
