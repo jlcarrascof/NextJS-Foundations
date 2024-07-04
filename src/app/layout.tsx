0e937c9ebe068d617230a0d27d45d03cf287561d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-03 23:04:10 -0400

    chore: Add page module CSS and update layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
