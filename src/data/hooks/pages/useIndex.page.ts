225d4886f978725173c8b17be04138ffb8fe41f2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-01 14:27:15 -0400

    feat: Add 'How to create a NextJS app' to the list in useIndex.page.ts

--- src/data/hooks/pages/useIndex.page.ts
+++ src/data/hooks/pages/useIndex.page.ts
@@ -2,6 +2,6 @@ import { BlogPost } from "@/data/@types/BlogPostInterface";
 
 export default function useIndex() {
     const posts: BlogPost[] = [];
-    let list = ['5 tips for use Github', 'How install VSCode'];
+    let list = ['5 tips for use Github', 'How install VSCode', 'How to create a NextJS app'];
     return { list };
 }
